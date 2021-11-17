0a8c90f72ab64f8a1ae2b3f65acb11fa5cecfe1d
lazyprogrammer-htx <84111005+lazyprogrammer-htx@users.noreply.github.com>
2021-11-17 10:50:41 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-//check out https://stackoverflow.com/questions/34219778/cant-find-an-element-by-queryselector to get elements inside the iframe (need to filter which iframe it is though!)
-
-
 let assignedElement = null;
 let serviceUnitStops = null;
 let tooltipDivs = null;
 let assigning = false;
 
 printLog("Loaded up cuz");
+
 
 // looking for keystrokes
 // Insert = assign new element
@@ -18,34 +16,79 @@ document.addEventListener("keydown", (e) => {
         printLog("Click any element to assign it");
         assigning = true;
     }else if(key === "\\"){
+        
+        // //test code vv
+        // const query = document.querySelector("#service_order_address_line1").value + ", " +
+        // document.querySelector("#service_order_city").value + ", " +
+        // document.querySelector("#service_order_state").value + ", " +
+        // document.querySelector("#service_order_zipcode").value;
+        // printLog("search this-> "+query);
+
+
+        //keep this
         click();
     }else{
-        printLog("Other key pressed");
+        printLog("Other key pressed " + key);
+        // printLog(e);
+    }
+});
+/*
+document.addEventListener("dblclick", (e) => {
+    if (document.activeElement.id === "edit_setup_time"){
+        document.activeElement.value = 12;
     }
 });
 
-// document.addEventListener("keydown", (e) => {
-//     const key = e.key;
-//     console.log("**testing -> Key pressed: " + key);
-//     if(key === "`"){
-//        const query = document.querySelector("#service_order_address_line1").value + ", " +
-//         document.querySelector("#service_order_city").value + ", " +
-//         document.querySelector("#service_order_state").value + ", " +
-//         document.querySelector("#service_order_zipcode").value;
-//         console.log("search this-> "+query);
-//     }
-// });
 
 document.addEventListener("click", (e) => {
     const clickTarget = e.target;
     if(assigning){
         assign(clickTarget);
         assigning = false;
+    }else if (document.activeElement.id === "edit_setup_time"){
+        printLog("detected setuptime input box!");
+        const setupTimeInputBox = document.activeElement;
+
+        const form = setupTimeInputBox.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
+        const lockButtonList = form.querySelectorAll(".service_time_lock");
+        const submitButtonList = form.querySelectorAll("#submitButtonSetupTime");
+        const lockButton = lockButtonList[0];
+        const submitButton = submitButtonList[0];
+
+        printLog(form);
+        printLog(lockButton);
+        printLog(submitButton);
+
+        const temp = parseInt(setupTimeInputBox.value);
+        setupTimeInputBox.value = temp + 15;
+
+        setupTimeInputBox.addEventListener("keydown", (e) => {
+            const key = e.key;
+            if(key === "Enter"){
+                
+                lockButton.dispatchEvent(new MouseEvent('click',{
+                    bubbles: true,
+                    cancelable: true,
+                    view: window
+                }));
+                // submitButton.dispatchEvent(new MouseEvent('click',{
+                //     bubbles: true,
+                //     cancelable: true,
+                //     view: window
+                // }));
+                e.preventDefault();
+            }
+        });
+        setupTimeInputBox.removeEventListener("keydown", (e) => { });
+
+        //lock button to click <a href="#" class="pull-left service_time_lock mt-10 routeUnlocked" style="cursor:pointer;width: 14px;height: 14px;" title="Service Time is Locked. Click to Unlock"></a>
+        //submit button <input class="btn btn-primary" id="submitButtonSetupTime" name="commit" type="submit" value="Save">
     }
 });
+*/
 
 function printLog(str){
-    console.log("***** Extension Log out *****");
+    console.log("***** CBE *****");
     console.log(str);
     console.log("**********");
 }
@@ -106,3 +149,46 @@ function assign(el){
     // }
     printLog("Assignment succesful");
 }
+
+
+// document.addEventListener("keydown", (e) => {
+//     const key = e.key;
+//     console.log("**testing -> Key pressed: " + key);
+//     if(key === "\\"){
+//        const query = document.querySelector("#service_order_address_line1").value + ", " +
+//         document.querySelector("#service_order_city").value + ", " +
+//         document.querySelector("#service_order_state").value + ", " +
+//         document.querySelector("#service_order_zipcode").value;
+//         console.log("search this-> "+query);
+//     }
+// });
+
+
+
+
+// else if(key ==="="){
+//     // printLog(" = pressed")
+//     const inputBox = document.activeElement;
+//     printLog(inputBox);
+//     if(inputBox.id === "edit_setup_time"){
+//         printLog("in the setup time input");
+//         // inputBox.parentElement.focus();
+//         // inputBox.value = 0;
+
+//         let setTime = null;
+//         //while true
+//         inputBox.addEventListener("keydown",(e) => {
+//             const key = e.key;
+//             printLog("insid event listenter" + key);
+            
+//             if(key === "1"){
+//                 setTime = 27;
+//             }
+//         });
+        
+//         inputBox.removeEventListener("keydown", (e) => { });
+
+//         inputBox.value = setTime;
+
+//     }
+// }
